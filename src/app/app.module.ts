ef1ae6c75e5b69de2cdaa7d43e636edc7038e34b
Sidhant <biswalsidhant.spb@gmail.com>
2023-09-27 17:53:22 +0530

    product details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
+    
     BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {
+        path:'',component:ProductListComponent
+      },
+      {
+        path:'products/:productId',component:ProductDetailsComponent
+      }
+    ]),
     AppRoutingModule
   ],
+
+  // declarations:[
+
+  // ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,23 @@
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products,IProduct } from '../Products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  ProdDetail:IProduct | undefined;
+  constructor(private route: ActivatedRoute){}
+  ngOnInit(){
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number (routeParams.get('productId'));
+
+    // find the product that corresponds with the id provided in the route
+
+    this.ProdDetail = products.find(p => p.id === productIdFromRoute);
+
+    window.alert(this.ProdDetail)
+  }
+}
